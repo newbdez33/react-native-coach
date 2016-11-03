939cedf711537ebe439e89988861372fca4e9f5e
Jacky <newbdez33@gmail.com>
2016-11-03 22:25:02 +0900

    router

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 
 import App from './modules/App'
+import Job from './modules/Job'
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App} />
+    <Route path="/j/:jid" component={Job}/>
   </Router>
 ), document.getElementById('app'))
--- src/modules/App.js
+++ src/modules/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default React.createClass({
   render() {
-    return <div>React WEB by Jacky</div>
+    return <div>
+                React WEB by Jacky<br />
+                <div><a href="./#/j/1">Go to Job module</a></div>
+            </div>
+
   }
 })
